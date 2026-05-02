7013cf37edb1576d6e5e8ebc63f2d2dc50fc0ab1
AndrewGogo <gogoboot@yandex>
2026-05-02 12:08:47 +0300

    feat: centralize auth token management

--- client/src/presentation/components/videoLayout.js
+++ client/src/presentation/components/videoLayout.js
@@ -3,39 +3,46 @@
  */
 
 export function createVideoLayout({ mainVideo, pipVideo, pipOverlay }) {
-    let isRemoteOnMain = true;
+  let isRemoteOnMain = true;
 
-    function update({ localStream, remoteStream }) {
-        const hasLocal  = !!localStream;
-        const hasRemote = !!remoteStream;
+  function update({ localStream, remoteStream }) {
+    const hasLocal = !!localStream;
+    const hasRemote = !!remoteStream;
 
-        if (hasLocal && hasRemote) {
-            mainVideo.srcObject = isRemoteOnMain ? remoteStream : localStream;
-            pipVideo.srcObject  = isRemoteOnMain ? localStream  : remoteStream;
-            pipOverlay.classList.remove('hidden');
-        } else if (hasLocal) {
-            mainVideo.srcObject = localStream;
-            pipVideo.srcObject  = null;
-            pipOverlay.classList.add('hidden');
-            isRemoteOnMain = true;
-        } else {
-            mainVideo.srcObject = null;
-            pipVideo.srcObject  = null;
-            pipOverlay.classList.add('hidden');
-        }
+    if (hasLocal && hasRemote) {
+      mainVideo.srcObject = isRemoteOnMain ? remoteStream : localStream;
+      pipVideo.srcObject = isRemoteOnMain ? localStream : remoteStream;
+      pipOverlay.classList.remove("hidden");
+    } else if (hasLocal && !hasRemote) {
+      mainVideo.srcObject = localStream;
+      pipVideo.srcObject = null;
+      pipOverlay.classList.add("hidden");
+      isRemoteOnMain = true;
+    } else if (hasRemote && !hasLocal) {
+      // ← ДОБАВИТЬ: только удалённый (нет камеры или занята)
+      mainVideo.srcObject = remoteStream;
+      pipVideo.srcObject = null;
+      pipOverlay.classList.add("hidden");
+      isRemoteOnMain = true;
+    } else {
+      mainVideo.srcObject = null;
+      pipVideo.srcObject = null;
+      pipOverlay.classList.add("hidden");
+      isRemoteOnMain = true;
     }
+  }
 
-    function swap() {
-        isRemoteOnMain = !isRemoteOnMain;
-        return isRemoteOnMain;
-    }
+  function swap() {
+    isRemoteOnMain = !isRemoteOnMain;
+    return isRemoteOnMain;
+  }
 
-    function reset() {
-        mainVideo.srcObject = null;
-        pipVideo.srcObject  = null;
-        pipOverlay.classList.add('hidden');
-        isRemoteOnMain = true;
-    }
+  function reset() {
+    mainVideo.srcObject = null;
+    pipVideo.srcObject = null;
+    pipOverlay.classList.add("hidden");
+    isRemoteOnMain = true;
+  }
 
-    return { update, swap, reset };
+  return { update, swap, reset };
 }
